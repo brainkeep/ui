815e61eaea07d35f1227bec16fa577ae7ba1c133
Junpu Fan <junpu.fan@target.com>
2019-01-21 15:25:08 -0600

    comment change

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-// React Redux
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
-// Material UI
 import CssBaseline from '@material-ui/core/CssBaseline';
-
-// Store
 import store from './app/store';
-
-// Containers
 import App from './app/App';
-
-/*
-
- █████╗ ██████╗ ██████╗     ██████╗  ██████╗  ██████╗ ████████╗
-██╔══██╗██╔══██╗██╔══██╗    ██╔══██╗██╔═══██╗██╔═══██╗╚══██╔══╝
-███████║██████╔╝██████╔╝    ██████╔╝██║   ██║██║   ██║   ██║   
-██╔══██║██╔═══╝ ██╔═══╝     ██╔══██╗██║   ██║██║   ██║   ██║   
-██║  ██║██║     ██║         ██║  ██║╚██████╔╝╚██████╔╝   ██║   
-╚═╝  ╚═╝╚═╝     ╚═╝         ╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝   
-                                                               
-*/
 
 render (
     <Provider store={store}>
