af5a38579a7fdd5cec1c9a7f630a66e47f56df31
junpufan <junpufan>
2019-02-07 22:43:30 -0600

    rename

--- src/app/ResourcePaths.js
+++ src/app/ResourcePaths.js
@@ -1,7 +1,7 @@
-export const FETCH_IN_REVIEW_PROBLEMS_PATH = "leetsspellbook/problem/queued/true/all";
-export const UPDATE_IN_REVIEW_PROBLEMS_PATH = "leetsspellbook/problem/review";
-export const FETCH_REVIEW_QUEUE_PATH = "leetsspellbook/review/queue/all";
-export const UPDATE_REVIEW_QUEUE_PATH = "leetsspellbook/review/queue/one";
-export const FETCH_CODER_PATH = "leetsspellbook/coder/one";
-export const SAVE_NEW_PROBLEM_PATH = "leetsspellbook/problem/create";
-export const FETCH_PROBLEMS_PATH = "leetsspellbook/problem/all";
+export const FETCH_IN_REVIEW_PROBLEMS_PATH = "kurumi/problem/queued/true/all";
+export const UPDATE_IN_REVIEW_PROBLEMS_PATH = "kurumi/problem/review";
+export const FETCH_REVIEW_QUEUE_PATH = "kurumi/review/queue/all";
+export const UPDATE_REVIEW_QUEUE_PATH = "kurumi/review/queue/one";
+export const FETCH_CODER_PATH = "kurumi/coder/one";
+export const SAVE_NEW_PROBLEM_PATH = "kurumi/problem/create";
+export const FETCH_PROBLEMS_PATH = "kurumi/problem/all";
